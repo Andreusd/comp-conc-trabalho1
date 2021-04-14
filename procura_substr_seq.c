c8acf8cae77dad3c3420e1abaccbccf1e69a62e5
Hashi364 <49736221+Kiyoshi364@users.noreply.github.com>
2021-04-13 21:25:10 -0300

    procura_substr_seq: mudanças estéticas

--- procura_substr_seq.c
+++ procura_substr_seq.c
@@ -2,45 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-int procuraSubstr(char * string, char * substring) {
+#define TAM_BUF 100000
+
+int procuraSubstr(char *string, char *substring) {
     int tamString = strlen(string);
     int tamSubstring = strlen(substring);
-    printf("procurando %s\ntamanho da string: %d\n",substring,tamString); //debug
-    for(int i=0;i<tamString;i++) {
-        if(string[i]==substring[0]) {
-            for(int j=0;j<tamSubstring;j++) {
-                if(string[i+j]!=substring[j]) {
+    printf("procurando: %s\ntamanho da string: %d\n", substring, tamString); //debug
+    for ( int i = 0; i < tamString; i++ ) {
+
+        if ( string[i] == substring[0] ) {
+            for ( int j = 0; j < tamSubstring; j++ ) {
+                if ( string[i+j] != substring[j] ) {
                     break;
                 }
-                if(j==tamSubstring-1) {
-                    printf("achei!");
+                if ( j == tamSubstring-1 ) {
+                    printf("achei!\n");
                     return i;
                 }
             }
         }
+
     }
     return -1;
 }
 
+int main(int argc, char **argv) {
+  char textoArquivo[TAM_BUF];
+  char *stringProcurada;
 
-int main(int argc, char * argv[]) {
-  char textoArquivo[100000];
-  char stringProcurada[50];
-
-  if(argc<2) {
-    fprintf(stderr,"digite %s <substring>\n",argv[0]);
+  if ( argc < 2 ) {
+    fprintf(stderr, "digite %s <substring>\n", argv[0]);
     return 1;
   }
 
-  strcpy(stringProcurada,argv[1]);
+  stringProcurada = argv[1];
 
-  FILE *fptr;
-  if((fptr = fopen("arquivo_alvo.txt","r")) == NULL) {
-      printf("Erro na abertura!");   
+  FILE *fptr = fopen("arquivo_alvo.txt", "r");
+  if ( fptr == NULL ) {
+      printf("Erro na abertura!\n");   
       exit(1);             
   }
-  fscanf(fptr,"%s", textoArquivo);
+  fgets(textoArquivo, TAM_BUF, fptr);
 
-  int index = procuraSubstr(textoArquivo,stringProcurada);
-  printf("%d",index);
+  int index = procuraSubstr(textoArquivo, stringProcurada);
+  printf("%d\n", index);
 }
