3eb26c98c03483f202da0f3bfb0e31a26c24af91
Hashi364 <49736221+Kiyoshi364@users.noreply.github.com>
2021-04-13 21:50:58 -0300

    procura_substr_seq: mudanças no algoritmo e mais comentários

--- procura_substr_seq.c
+++ procura_substr_seq.c
@@ -5,20 +5,30 @@
 #define TAM_BUF 100000
 
 int procuraSubstr(char *string, char *substring) {
-    int tamString = strlen(string);
-    int tamSubstring = strlen(substring);
+    int tamString = strlen(string); // debug
     printf("procurando: %s\ntamanho da string: %d\n", substring, tamString); //debug
-    for ( int i = 0; i < tamString; i++ ) {
 
+    // Até o final da string
+    for ( int i = 0; string[i] != '\0'; i++ ) {
+
+        // Se é um possível começo
         if ( string[i] == substring[0] ) {
-            for ( int j = 0; j < tamSubstring; j++ ) {
+            int j;
+
+            // Até o final da substring
+            for ( j = 0; substring[j] != '\0'; j++ ) {
+
+                // Se está diferente, i = 0 e para
                 if ( string[i+j] != substring[j] ) {
+                    j = 0;
                     break;
                 }
-                if ( j == tamSubstring-1 ) {
-                    printf("achei!\n");
-                    return i;
-                }
+            }
+
+            // Se chegamos ao final da substring, achamos uma posição!
+            if ( substring[j] == '\0' ) {
+                printf("achei!\n");
+                return i;
             }
         }
 
@@ -39,11 +49,15 @@ int main(int argc, char **argv) {
 
   FILE *fptr = fopen("arquivo_alvo.txt", "r");
   if ( fptr == NULL ) {
-      printf("Erro na abertura!\n");   
-      exit(1);             
+      printf("Erro na abertura!\n");
+      exit(1);
   }
   fgets(textoArquivo, TAM_BUF, fptr);
 
+  fclose(fptr);
+
   int index = procuraSubstr(textoArquivo, stringProcurada);
   printf("%d\n", index);
+
+  return 0;
 }
