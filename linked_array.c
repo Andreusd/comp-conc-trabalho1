ec400b7c826a34055e00039c083cb50f5464a315
Hashi364 <49736221+Kiyoshi364@users.noreply.github.com>
2021-04-25 14:40:32 -0300

    linkedArray: comentários

--- linked_array.c
+++ linked_array.c
@@ -4,6 +4,7 @@ typedef struct _linkedArray {
     struct _linkedArray * next;
 } linkedArray;
 
+// Alloca um novo linkedArray
 linkedArray* novo_linkedArray() {
     linkedArray *la;
     la = (linkedArray *) malloc( sizeof( linkedArray ) );
@@ -16,6 +17,7 @@ linkedArray* novo_linkedArray() {
     return la;
 }
 
+// Desalloca um linkedArray e seus `próximos`
 void destroy_linkedArray(linkedArray *la) {
     while ( la ) {
         linkedArray *next = la->next;
@@ -24,32 +26,44 @@ void destroy_linkedArray(linkedArray *la) {
     }
 }
 
+// Insere um valor num linkedArray
 int push_linkedArray(linkedArray *la, int n) {
+    // Testando se linkedArray existe
     if ( !la ) {
         return -1; 
     }
+    // Se não cabe...
     if ( la->pos >= 50 ) {
+        // Cria um próximo, se ele não existe
         if ( la->next == NULL ) {
             linkedArray * next = novo_linkedArray();
             la->next = next;
         }
+        // Pede para o próximo guardar
         push_linkedArray( la->next, n );
     } else {
+        // Aqui cabe, só precisa inserir
         (la->data)[la->pos++] = n;
     }
     return 0;
 }
 
+// Pega um valor, similar a array[pos]
 int get_linkedArray(linkedArray * la, int pos) {
+    // Testando se linkedArray existe
     if ( !la ) {
         return -1; 
     }
+    // Se está no próximo...
     if ( pos >= 50 ) {
+        // Testando se existe próximo
         if ( la->next == NULL ) {
             return -1;
         }
+        // Pede para o próximo
         return get_linkedArray( la->next, pos-50 );
     } else {
+        // Está aqui, só retorna
         return (la->data)[pos];
     }
 }
